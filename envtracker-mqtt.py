57953abb6b19cf53c56de1fed7d2940dc51d18dc
Mohammed Abubaker <withabubaker@hotmail.com>
2025-04-19 21:39:23 -0400

    clean the code

--- envtracker-mqtt.py
+++ envtracker-mqtt.py
@@ -8,9 +8,10 @@ import boto3
  
 MQTT_Broker = '192.168.2.75'  # Thingsboard MQTT broker address
 S1_ACCESS_TOKEN =os.getenv('S1_ACCESS_TOKEN')
+topic_arn = os.getenv('TOPIC_ARN')
 url = "http://192.168.2.75:3001/devices/ac233fa4d282/2"
 sns = boto3.client('sns', region_name='us-east-1')
-topic_arn = os.getenv('TOPIC_ARN')
+
 
 def fetch_temp_hum(url):
     try:
@@ -35,35 +36,25 @@ def update_temp_Hum_dashboard(client_S1,payload):
         if payload is None:
             print("No payload to send")
             return
-
         topic = "v1/devices/me/telemetry"
         client_S1.publish(topic, json.dumps(payload))
-        print("Data sent to ThingBoard:", payload)
-
-        """
-        if payload['temperature'] > 30:
-            message = f"Temperature Alert: {payload['temperature']}°C"
-            sns.publish(TopicArn=topic_arn, Message=message)
-            print("Alert sent to SNS:", message)
-        if payload['humidity'] > 40:
-            message = f"Humidity Alert: {payload['humidity']}%"
-            sns.publish(TopicArn=topic_arn, Message=message)
-            print("Alert sent to SNS:", message)
-        """
-            
+        print("Data sent to ThingBoard:", payload) 
     except Exception as e:
         print("An error occurred:", e)
+
+
+
 def sns_notification(payload):
     try:
         if payload is None:
             print("No data")
             return
-        if payload['temperature'] > 30:
-            message = f"High Temperature Alert: {payload['temperature']}°C"
+        if payload['temperature'] > 24:
+            message = f"High Temperature Alert: {round(payload['temperature'])} °C"
             sns.publish(TopicArn=topic_arn, Message=message)
             print("Alert sent to SNS:", message)
         if payload['humidity'] > 40:
-            message = f"High Humidity Alert: {payload['humidity']}R/H"
+            message = f"High Humidity Alert: {round(payload['humidity'])} R/H"
             sns.publish(TopicArn=topic_arn, Message=message)
             print("Alert sent to SNS:", message)
     except Exception as e:
